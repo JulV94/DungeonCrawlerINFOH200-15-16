22d61645cc82d924e65bdc8d448407815663338e
JulV <julvdev@gmail.com>
2016-03-07 17:41:34 +0100

    project in Intellij

---
+++ src/com/gmail/julvdev/DungeonCrawler/DungeonCrawler.java
@@ -0,0 +1,10 @@
+package com.gmail.julvdev.DungeonCrawler;
+
+/**
+ * Created by julien on 7/03/16.
+ */
+public class DungeonCrawler {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
